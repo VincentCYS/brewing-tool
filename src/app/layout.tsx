5d31871df9c3260611ebcb775c288c04a3f98434
VincentCYS <vincent77674@gmail.com>
2025-05-03 21:33:55 +0800

    update adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,14 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<script
-				async
-				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4601799641974929"
-				crossorigin="anonymous"
-			></script>
+			<head>
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4601799641974929"
+					crossorigin="anonymous"
+				></script>
+			</head>
+
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				{children}
 			</body>
